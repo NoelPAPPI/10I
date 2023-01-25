7b0bf9f5ccf9de1344d43ef85df1cb622afc06a7
NoelPAPPI <87492824+NoelPAPPI@users.noreply.github.com>
2023-01-25 10:29:56 +0100

    Add files via upload

---
+++ beadando.py
@@ -0,0 +1,7 @@
+valtozo = int(input("Kérem az első számot: "))
+valtozo2 = int(input("Kérem a második számot: "))
+
+if valtozo > valtozo2:
+    print(valtozo, " ez a nagyobb szám")
+else:
+    print(valtozo2, " ez a nagyobb szám")
